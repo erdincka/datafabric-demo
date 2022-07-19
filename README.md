48b21f985636168dde67b9caf47a3a0a4a41ea08
Erdinc Kaya <erdincka@msn.com>
2022-07-19 18:59:56 +0300

    few updates on server install - work in progress

---
+++ README.md
@@ -0,0 +1,29 @@
+# Data Fabric Demo Env
+
+AWS Ubuntu 20.04 AMI.
+
+Use keypair for passwordless access.
+
+## Server Installation Notes
+
+Select an instance with 16 core 64GB Memory.
+
+60GB OS disk + 30GB data disk (or size of your working data set x3)
+
+Use ./df-in-ubuntu.sh to install.
+
+## Client Installation Notes
+
+Select an instance with 4 core 16GB Memory.
+
+30GB OS disk
+
+Use ./client-on-ubuntu.sh to install.
+
+## Work in progress
+
+Let me know any issues.
+
+## TODO
+
+Client for M1 Macs.
--- df-on-ubuntu.sh
+++ df-on-ubuntu.sh
@@ -10,6 +10,8 @@ export MAPR_GROUP=mapr
 export MAPR_USER_HOME=/home/mapr
 export MAPR_MOUNT_PATH=/mapr
 export MAPR_DATA_PATH=/data
+export MAPR_HOST=core.demo.df 
+export MAPR_CLUSTER=core.demo.df
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -27,13 +29,13 @@ sudo apt install -y --no-install-recommends gnupg2 iproute2 \
     libltdl7 libpython2.7 irqbalance iputils-arping iputils-ping iputils-tracepath dmidecode hdparm sdparm \
     default-jdk openssh-client wamerican lsb-release apt-utils rpcbind nfs-common cron
 
-sudo sed -i 's/#PasswordAuthentication/PasswordAuthentication/' /etc/ssh/sshd_config; \
-    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config; \
-    sudo sed -i 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' /etc/pam.d/sshd; \
-    sudo /usr/bin/ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa; \
-    /usr/bin/ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa; \
-    cp $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys; \ 
-    sudo cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys 
+sudo sed -i 's/#PasswordAuthentication/PasswordAuthentication/' /etc/ssh/sshd_config
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo sed -i 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' /etc/pam.d/sshd
+sudo /usr/bin/ssh-keygen -t rsa -N "" -f /root/.ssh/id_rsa
+/usr/bin/ssh-keygen -t rsa -N "" -f $HOME/.ssh/id_rsa
+cp $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys
+sudo cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys 
 
 wget -O - https://package.mapr.hpe.com/releases/pub/maprgpg.key | sudo apt-key add -
 echo 'deb https://package.mapr.hpe.com/releases/v7.0.0/ubuntu binary bionic' | sudo tee -a /etc/apt/sources.list
@@ -53,22 +55,19 @@ sudo apt install -y mapr-fileserver \
 
 sudo groupadd -g ${MAPR_GID} ${MAPR_GROUP} && sudo useradd -m -u ${MAPR_UID} -g ${MAPR_GID} -d ${MAPR_USER_HOME} -s /bin/bash ${MAPR_USER} && sudo usermod -a -G sudo ${MAPR_GROUP}
 echo "${MAPR_USER}:${MAPR_PASS}" | sudo chpasswd 
-sudo sed -i 's!/proc/meminfo!/opt/mapr/conf/meminfofake!' /opt/mapr/server/initscripts-common.sh    
 
 sudo mkdir ${MAPR_MOUNT_PATH}
 sudo mkdir ${MAPR_DATA_PATH}
 
-export MAPR_HOST=local.demo.df 
-export MAPR_CLUSTER=local.demo.df
 echo "${MAPR_HOST}" | sudo tee /etc/hostname 
 
-IP=$(/sbin/ip -o -4 addr list enp0s7 | awk '{print $4}' | cut -d/ -f1)
+IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 HOSTNAME=$(hostname -f)
 head -n -1  /etc/hosts > tmp.txt && sudo cp tmp.txt /etc/hosts && rm tmp.txt
 echo "$IP  ${MAPR_HOST} ${HOSTNAME}" | sudo tee -a /etc/hosts
 echo "session       required       pam_limits.so" | sudo tee -a /etc/pam.d/common-session
 
-sudo /opt/mapr/server/configure.sh -N ${MAPR_HOST} -C ${MAPR_HOST}:7222 -Z ${MAPR_HOST} -u ${MAPR_USER} -g ${MAPR_GROUP} -genkeys
+sudo /opt/mapr/server/configure.sh -N ${MAPR_HOST} -C ${MAPR_HOST}:7222 -Z ${MAPR_HOST} -u ${MAPR_USER} -g ${MAPR_GROUP} -genkeys -secure -dare
 
 apt install -y mapr-posix-client-basic
 service mapr-posix-client-basic start
