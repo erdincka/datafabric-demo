34871e8f495a3a9133818803193109183f27c116
Erdinc Kaya <erdincka@msn.com>
2022-09-14 14:31:50 +0100

    use IP for cluster configure

--- start-server.sh
+++ start-server.sh
@@ -15,7 +15,7 @@ head -n -1  /etc/hosts > tmp.txt && cp tmp.txt /etc/hosts && rm tmp.txt
 echo "$IP  ${MAPR_CLUSTER} ${HOSTNAME}" >> /etc/hosts
 echo "session       required       pam_limits.so" >> /etc/pam.d/common-session
 
-/opt/mapr/server/configure.sh -N ${MAPR_CLUSTER} -C ${MAPR_CLUSTER}:7222 -Z ${MAPR_CLUSTER} -u ${MAPR_USER} -g ${MAPR_GROUP} -genkeys -secure -dare
+/opt/mapr/server/configure.sh -N ${MAPR_CLUSTER} -C ${IP}:7222 -Z ${IP} -u ${MAPR_USER} -g ${MAPR_GROUP} -genkeys -secure -dare
 
 # apt install -y mapr-posix-client-basic
 # service mapr-posix-client-basic start
